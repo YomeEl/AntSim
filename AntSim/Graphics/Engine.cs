609bd1fcc5bd8cb85ace49ad0a782e12b0110be9
Ярослав <umpepatopChannel@gmail.com>
2020-02-08 15:40:51 +0500

    Removed old drawing

--- AntSim/Graphics/Engine.cs
+++ AntSim/Graphics/Engine.cs
@@ -10,7 +10,6 @@ namespace AntSim.Graphics
         private readonly RenderWindow win;
         private byte cellSize;
         private Vector2i previousMousePosition;
-        private const bool RENDER_SMELLS = false;
 
         public Camera Camera { get; }
 
@@ -96,52 +95,6 @@ namespace AntSim.Graphics
                 {
                     var pos = new Vector2i(obj.Position.X * cellSize - offset.X + (int)win.Size.X / 2, obj.Position.Y * cellSize - offset.Y + (int)win.Size.Y / 2);
                     obj.Draw(win, pos, cellSize);
-                }
-            }
-
-            win.Display();
-
-            win.DispatchEvents();
-
-            return win.IsOpen;
-        }
-
-        //Deprecated drawing
-        public bool Draw(Field<Cell> field)
-        {
-            RecalculateCameraParameters();
-
-            win.Clear(Color.White);
-
-            int maxAntSize = 4;
-
-            float trueLeft = Camera.Position.X - win.Size.X / 2;
-            float trueTop = Camera.Position.Y - win.Size.Y / 2;
-            int left = (int)(trueLeft / cellSize);
-            int top = (int)(trueTop / cellSize);
-            int width = (int)win.Size.X / cellSize + 1;
-            int height = (int)win.Size.Y / cellSize + 1;
-            (int X, int Y) offset = ((int)Camera.Position.X, (int)Camera.Position.Y);
-
-            var rect = new RectangleShape(new Vector2f(cellSize, cellSize));
-            for (int i = left - maxAntSize; i < left + width; i++)
-            {
-                for (int j = top - maxAntSize; j < top + height; j++)
-                {
-                    if (field[i, j].Entity != null)
-                    {
-                        var pos = new Vector2i(i * cellSize - offset.X + (int)win.Size.X / 2, j * cellSize - offset.Y + (int)win.Size.Y / 2);
-                        field[i, j].Entity.Draw(win, pos, cellSize);
-                    }
-
-                    if (RENDER_SMELLS && field[i, j].Smells.ContainsKey(0))
-                    {
-                        var pos = new Vector2i(i * cellSize - offset.X + (int)win.Size.X / 2, j * cellSize - offset.Y + (int)win.Size.Y / 2);
-                        float alpha = 255f * field[i, j].Smells[0] / 100f;
-                        rect.FillColor = new Color(255, 0, 0, (byte)alpha);
-                        rect.Position = new Vector2f(pos.X, pos.Y);
-                        win.Draw(rect);
-                    }
                 }
             }
 
