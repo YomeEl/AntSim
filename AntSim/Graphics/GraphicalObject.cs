fe76990eedb3317b16f8fb5992f2f559dc289c3f
Ярослав <umpepatopChannel@gmail.com>
2021-04-04 16:45:38 +0500

    Implemented IComparable interface in GraphicalObject

--- AntSim/Graphics/GraphicalObject.cs
+++ AntSim/Graphics/GraphicalObject.cs
@@ -1,21 +1,43 @@
 ﻿using SFML.Graphics;
 using SFML.System;
 
-using AntSim.Simulation.Ants;
+using System;
 
 namespace AntSim.Graphics
 {
-    class GraphicalObject
+    class GraphicalObject : IComparable
     {
         public Texture Texture { get; }
         public (byte W, byte H) Size { get; }
         public Vector2f Position { get; set; }
         public float Rotation { get; set; } = 0f;
 
+        private static int nextId = 0;
+        private int id;
+
         public GraphicalObject(Texture texture, byte width, byte height)
         {
             Texture = texture;
             Size = (width, height);
+
+            id = nextId++;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+
+            if (obj is GraphicalObject)
+            {
+                return id.CompareTo(((GraphicalObject)obj).id);
+            }
+            else
+            {
+                throw new Exception("Trying to compare objects of different types!");
+            }
         }
     }
 }
