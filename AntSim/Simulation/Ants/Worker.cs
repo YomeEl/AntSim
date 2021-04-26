1b36ed90b87c03fe659c4a25d5bcb0b0ca2903b1
Ярослав <umpepatopChannel@gmail.com>
2021-04-26 19:29:12 +0500

    Added waypoints

--- AntSim/Simulation/Ants/Worker.cs
+++ AntSim/Simulation/Ants/Worker.cs
@@ -1,4 +1,6 @@
-﻿using AntSim.Simulation.Map;
+﻿using System;
+
+using AntSim.Simulation.Map;
 
 using SFML.System;
 
@@ -7,28 +9,41 @@ namespace AntSim.Simulation.Ants
     class Worker : Ant
     {
         private const int SEARCHING_RADIUS = 10;
+        private const float LEG_LENGTH = 1;
+
+        private Vector2f waypoint;
+        private Vector2i? foodPosition;
 
         public Worker(uint antId, uint factionId, SFML.Graphics.Texture texture, byte width, byte height) : 
             base(antId, factionId, texture, width, height)
         {
+            waypoint = Position;
         }
 
         public override void Step(float dt, Field<Cell> field)
         {
-            var target = FindFarSmell(field, SEARCHING_RADIUS, Map.Smells.SmellType.FromFood);
-            float mult = 1f;
-            if (!target.found)
+            if (Math.Abs(waypoint.X - Position.X) < 0.1f &&
+                Math.Abs(waypoint.Y - Position.Y) < 0.1f)
             {
-                target.position.X = randomizer.Next(-100, 100);
-                target.position.Y = randomizer.Next(-100, 100);
-                mult = 0.0001f;
+                var target = FindFarSmell(field, SEARCHING_RADIUS, Map.Smells.SmellType.FromFood);
+                if (!target.found)
+                {
+                    waypoint.X = randomizer.Next(-SEARCHING_RADIUS, SEARCHING_RADIUS);
+                    waypoint.Y = randomizer.Next(-SEARCHING_RADIUS, SEARCHING_RADIUS);
+                    foodPosition = null;
+                }
+                else
+                {
+                    waypoint.X = target.position.X;
+                    waypoint.Y = target.position.Y;
+                    foodPosition = target.position;
+                }
+                waypoint += Position;
+
+                Direction = Normalize(waypoint - Position);
             }
 
-            var newDir = Direction + mult * new Vector2f(target.position.X, target.position.Y);
-            newDir = Normalize(newDir) * dt;
-
-            Position += newDir;
-            Direction = newDir;
+            Position += Direction * dt;
         }
     }
 }
