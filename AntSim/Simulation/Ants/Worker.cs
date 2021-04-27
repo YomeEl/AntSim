e8a12148d14e25a7144218af7a6b3eec6ce973c7
Ярослав <umpepatopChannel@gmail.com>
2021-04-27 10:58:19 +0500

    Changed randomizing waypoints

--- AntSim/Simulation/Ants/Worker.cs
+++ AntSim/Simulation/Ants/Worker.cs
@@ -8,8 +8,9 @@ namespace AntSim.Simulation.Ants
 {
     class Worker : Ant
     {
-        private const int SEARCHING_RADIUS = 10;
+        private const int SEARCHING_RADIUS = 25;
         private const int NEW_WP_MIN_DIST = 5;
+        private const int NEW_WP_MAX_DIST = 10;
 
         public Vector2f waypoint;
 
@@ -24,37 +25,41 @@ namespace AntSim.Simulation.Ants
 
         public override void Step(float dt, Field<Cell> field)
         {
-            var dist = Distance(waypoint, Position);
-            if (dist < 0.1f || dist > 2 * SEARCHING_RADIUS)
+            if (foodPosition == null)
             {
-                var target = FindFarSmell(field, SEARCHING_RADIUS, Map.Smells.SmellType.FromFood);
-                if (!target.found)
+
+                var dist = Distance(waypoint, Position);
+                if (dist < 0.1f || dist > 2 * SEARCHING_RADIUS)
                 {
-                    float sgnX = (randomizer.Next(0, 9) < 5) ? 1 : -1;
-                    float sgnY = (randomizer.Next(0, 9) < 5) ? 1 : -1;
-                    waypoint.X = randomizer.Next(NEW_WP_MIN_DIST, SEARCHING_RADIUS) * sgnX;
-                    waypoint.Y = randomizer.Next(NEW_WP_MIN_DIST, SEARCHING_RADIUS) * sgnY;
-                    foodPosition = null;
+                    var target = FindFarSmell(field, SEARCHING_RADIUS, Map.Smells.SmellType.FromFood);
+                    if (!target.found)
+                    {
+                        float sgnX = (randomizer.Next(0, 9) < 5) ? 1 : -1;
+                        float sgnY = (randomizer.Next(0, 9) < 5) ? 1 : -1;
+                        waypoint.X = randomizer.Next(NEW_WP_MIN_DIST, NEW_WP_MAX_DIST) * sgnX;
+                        waypoint.Y = randomizer.Next(NEW_WP_MIN_DIST, NEW_WP_MAX_DIST) * sgnY;
+                        foodPosition = null;
+                    }
+                    else
+                    {
+                        waypoint.X = target.position.X;
+                        waypoint.Y = target.position.Y;
+                        foodPosition = target.position;
+                    }
+                    waypoint += Position;
+
+                    Direction = Normalize(waypoint - Position);
                 }
                 else
                 {
-                    waypoint.X = target.position.X;
-                    waypoint.Y = target.position.Y;
-                    foodPosition = target.position;
+                    if (IsRotating)
+                    {
+                        Direction = Normalize(waypoint - Position);
+                    }
                 }
-                waypoint += Position;
 
-                Direction = Normalize(waypoint - Position);
+                Position += Direction * dt * speed;
             }
-            else
-            {
-                if (IsRotating)
-                {
-                    Direction = Normalize(waypoint - Position);
-                }
-            }
-
-            Position += Direction * dt * speed;
         }
     }
 }
