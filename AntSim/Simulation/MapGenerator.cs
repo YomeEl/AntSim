bd68b94aafd8a759ce86ff986e3c6af3b758d30f
Ярослав <umpepatopChannel@gmail.com>
2021-05-09 02:16:34 +0500

    New food pile generation algorythm

--- AntSim/Simulation/MapGenerator.cs
+++ AntSim/Simulation/MapGenerator.cs
@@ -15,6 +15,7 @@ namespace AntSim.Simulation
 
         private readonly Random randomizer;
         private readonly Graphics.Engine engine;
+        private const int FOOD_PILE_RADIUS = 20;
 
         public MapGenerator(Graphics.Engine engine)
         {
@@ -28,27 +29,57 @@ namespace AntSim.Simulation
         public Chunk<Cell> GenerateChunk(Vector2i position)
         {
             var chunk = new Chunk<Cell>();
-            var size = Chunk<Cell>.SIZE;
-
-            for (int i = 0; i < size; i++)
+            const int CHUNK_SIZE = Chunk<Cell>.SIZE;
+            var center = new Vector2i(1, 1) * (CHUNK_SIZE / 2);
+            for (int i = 0; i < CHUNK_SIZE; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < CHUNK_SIZE; j++)
                 {
                     chunk.Grid[i, j] = new Cell();
-                    if (randomizer.Next(0, 25000) == 0)
-                    {
-                        var foodPile = ObjectsFactory.CreateFoodPile();
-                        chunk.Grid[i, j].Item = foodPile;
-                        chunk.Grid[i, j].Smells.Add(new Map.Smells.SmellInfo(Map.Smells.SmellType.FromFood, 10));
-                        float posX = i + position.X * size;
-                        float posY = j + position.Y * size;
-                        foodPile.Position = new Vector2f(posX, posY);
-                        engine.Register(foodPile);
-                    }
                 }
             }
 
+            if (position.X != 0 && position.Y != 0 && randomizer.Next(0, 4) == 0)
+            {
+                GenerateFoodPile(chunk, position, center, FOOD_PILE_RADIUS);
+            }
+
             return chunk;
+        }
+
+        private void GenerateFoodPile(Chunk<Cell> chunk, Vector2i chunkPosition, Vector2i relativeFoodPosition, int radius)
+        {
+            if (radius == 0 || chunk.Grid[relativeFoodPosition.X, relativeFoodPosition.Y].Item != null)
+            {
+                return;
+            }
+
+            var food = ObjectsFactory.CreateFoodPile();
+            float posX = relativeFoodPosition.X + chunkPosition.X * Chunk<Cell>.SIZE;
+            float posY = relativeFoodPosition.Y + chunkPosition.Y * Chunk<Cell>.SIZE;
+            food.Position = new Vector2f(posX, posY);
+
+            chunk.Grid[relativeFoodPosition.X, relativeFoodPosition.Y].Item = food;
+            chunk.Grid[relativeFoodPosition.X, relativeFoodPosition.Y].Smells.Add(new Map.Smells.SmellInfo(Map.Smells.SmellType.Food, 1));
+
+            engine.Register(food);
+
+            if (randomizer.Next(0, 10) <= 7)
+            {
+                GenerateFoodPile(chunk, chunkPosition, relativeFoodPosition + new Vector2i(1, 0), radius - 1);
+            }
+            if (randomizer.Next(0, 10) <= 7)
+            {
+                GenerateFoodPile(chunk, chunkPosition, relativeFoodPosition + new Vector2i(-1, 0), radius - 1);
+            }
+            if (randomizer.Next(0, 10) <= 7)
+            {
+                GenerateFoodPile(chunk, chunkPosition, relativeFoodPosition + new Vector2i(0, 1), radius - 1);
+            }
+            if (randomizer.Next(0, 10) <= 7)
+            {
+                GenerateFoodPile(chunk, chunkPosition, relativeFoodPosition + new Vector2i(0, -1), radius - 1);
+            }
         }
     }
 }
